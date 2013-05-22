471003794459f73cd2bc5a88f00040fecf126d24
nemo <_strannick_@inbox.ru>
2013-05-22 21:03:41 +0400

    изменены пользователь и пароль для подключения к БД

--- fiesta/settings.py
+++ fiesta/settings.py
@@ -17,8 +17,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'fiesta',                      # Or path to database file if using sqlite3.
-        'USER': 'webuser',                      # Not used with sqlite3.
-        'PASSWORD': 'sps67',                  # Not used with sqlite3.
+        'USER': 'postgres',                      # Not used with sqlite3.
+        'PASSWORD': '1',                  # Not used with sqlite3.
         'HOST': 'localhost',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
